a78b7b4e7b235f5c423a3e58a86e1e5ca51b6e85
MattWise <ttam.esiw@gmail.com>
2014-08-22 12:50:51 -0400

    Created B14A and B14B functions.

--- DensitySimulation/testing.py
+++ DensitySimulation/testing.py
@@ -2,8 +2,11 @@ from __future__ import division,print_function
 
 import Functions, LinAl, Params
 import numpy as np
+import time
 
+np.set_printoptions(threshold=np.nan,linewidth=2000)
 
+"""
 iP = Params.InitialParameters(number=100)
 print(str(iP))
 dC = Params.DerivedConstants(iP)
@@ -54,3 +57,20 @@ wM = LinAl.WMatrix(dsctFcts)
 print("fully initialized:", wM.dFfullyInitialized)
 
 wM.initW0()
+"""
+
+def calcB14C():
+	zeros=np.zeros((500,500),dtype=np.complex128)
+	I=np.identity(500,dtype=np.complex128)
+	C2=np.concatenate((zeros,zeros,I,zeros,zeros),0)
+	C3=np.concatenate((zeros,zeros,zeros,I,zeros),0)
+	C4=np.concatenate((zeros,zeros,zeros,zeros,I),0)
+	C5=np.concatenate((I,zeros,zeros,zeros,zeros),0)
+	columns=(C2,C3,C4,C5)
+	print()
+	return np.concatenate(columns,1)
+
+A=time.clock()
+print(calcB14C())
+B=time.clock()
+print(B-A)
