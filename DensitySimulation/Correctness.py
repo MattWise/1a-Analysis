4e3f68c7e50aa89e6faa66eee77b73ab83c801f6
Martin L <starwars31337@gmail.com>
2014-09-02 23:44:06 +0200

    Add Correctness module and further testing.
    - Correctness module in order to put all the testing methods in there.
    - further testing attempts.

---
+++ DensitySimulation/Correctness.py
@@ -0,0 +1,8 @@
+""" This module covers the testing, debug and correctness implementations we did. Thus, it includes mathematical
+(e.g. numerical) and physical testing functions encapsulated in classes needed for that purpose.
+"""
+
+class Correctness(object):
+	def __init__(self):
+		# to be done later
+		pass
